50e148a6d2b084799f398d93f1400ca96a0f794e
lacie-life <00sao00ios00@gmail.com>
2022-01-19 17:23:38 +0700

    [Update] ros package

---
+++ src/CameraSync/camerasync.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <pthread.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+using namespace cv;
+using namespace std;
+
+static pthread_mutex_t foo_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct thread_data
+{
+    std::string path;
+    int thread_id;
+    std::string window_title;  
+};
+
+void *capture(void *threadarg)
+{
+    struct thread_data *data;
+    data = (struct thread_data *) threadarg;
+
+    cv::VideoCapture cap;
+
+    // Safely open video stream
+    pthread_mutex_lock(&foo_mutex);
+    cap.open(data->path, cv::CAP_GSTREAMER);
+    pthread_mutex_lock(&foo_mutex);
+
+    if (!cap.isOpened())
+    {
+        std::cout << "Camera " << data->thread_id << " open failed" << std::endl;
+    }
+
+    std::cout << "Camera " << data->thread_id << " open successed" << std::endl;
+
+    cv::Mat frame;
+    string ext = ".jpg";
+    std::string result;
+
+    // Create window with unique title
+    cv::namedWindow(data->window_title);
+
+    while(true)
+    {
+        cap >> frame;
+        std::cout << "Thread " << data->thread_id << std::endl;
+        cv::imshow(data->window_title, frame);
+        cv::waitKey(0);
+    }
+
+    // Release VideoCapture object
+    cap.release();
+    // Destroy previously created window
+    cv::destroyWindow(data->window_title);
+
+    // Exit thread 
+    pthread_exit(NULL);
+}
+
+int main(void)
+{
+    std::cout << "Junbot hello !!!" << std::endl;
+
+    const int thread_count = 2;
+
+    pthread_t threads[thread_count];
+    struct thread_data td[thread_count];
+
+    // Initialize thread data beforehand
+    td[0].path = "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+    td[0].window_title = "First Window";
+    td[1].path = "nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+    td[1].window_title = "Second Window";
+
+
+    int rc=0;
+    for( int i = 0; i < thread_count; i++ ) 
+    {
+        cout <<"main() : creating thread, " << i << endl;
+        td[i].thread_id = i;
+
+        rc = pthread_create(&(threads[i]), NULL, capture, (void *)& (td[i]) );
+
+        if (rc) 
+        {
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+
+    //Wait for the previously spawned threads to complete execution
+    for( int i = 0; i < thread_count; i++ )
+        pthread_join(threads[i], NULL);
+
+    pthread_exit(NULL);
+
+    return 0;
+}
---
+++ src/CameraSync/test.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <pthread.h>
+#include <thread>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+void writeFrametoDisk(const cv::Mat *frame, std::string path, int frameNum, std::string windowName)
+{
+  cv::imwrite(path.append(std::to_string(frameNum)).append(".png"), *frame);
+  return;
+}
+
+void openCameraStream(std::string path, std::string dirName, std::string title)
+{
+  cv::VideoCapture cap;
+  cap.open(path, cv::CAP_GSTREAMER);
+  if(!cap.isOpened()){std::cerr << "Unable to open the camera " << std::endl;}
+
+  std::cout << cap.get(cv::CAP_PROP_FRAME_WIDTH) << " " << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+
+  std::string windowName = title;
+  std::string outputDir = dirName;
+
+  bool frameStFg = false;
+  while(!frameStFg)
+  {
+    cv::Mat frame;
+    cap.read(frame);
+    if(frame.empty()){std::cerr << "frame buffer empty " << std::endl;}
+    else
+    {
+      frameStFg = true;
+    }
+  }
+
+  cv::TickMeter timer;
+  timer.start();
+  int frameCount = 0;
+
+  while(1)
+  {
+    cv::Mat frame;
+    cap.read(frame);
+
+    if(frame.empty()){std::cerr << "frame buffer empty " << std::endl;}
+    frameCount++;
+
+    std::thread th(writeFrametoDisk, &frame, outputDir, frameCount, windowName);
+    th.join();
+    //// a simple wayto exit the loop
+    if(frameCount > 500)
+    {
+      break;
+    }
+  }
+  timer.stop();
+  std::cout << "Device id " << title << " Capture ran for " << timer.getTimeSec() << " seconds" << std::endl;
+  std::cout << "Device id " << title << " Number of frames to be capture should be " << timer.getTimeSec() * 30 << std::endl;
+  std::cout << "Device id " << title << " Number of frames captured " << frameCount << std::endl;
+  cap.release();
+}
+
+int main(int argc, char * argv[])
+{
+  std::string outputDir1 = "left/";
+  std::string outputDir2 = "right/";
+  std::string cam0 = "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+  std::string cam1 = "nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+  std::string windowTitle0 = "Left";
+  std::string windowTitle1 = "Right";
+
+  std::thread camera1Thread(openCameraStream, cam0, outputDir1, windowTitle0);
+  std::thread camera2Thread(openCameraStream, cam1, outputDir2, windowTitle1);
+  camera1Thread.join();
+  camera2Thread.join();
+}
